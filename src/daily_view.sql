d9b1653b62682d7e583b3fdb11d0eac769bec8df
David G. Andersen <dave.andersen@gmail.com>
2024-05-25 23:20:46 +0000

    factor the json file access

--- src/daily_view.sql
+++ src/daily_view.sql
@@ -1,10 +1,13 @@
+CREATE OR REPLACE VIEW solar_log_json as
+SELECT * from read_json_auto('/home/dga/solar/log.json', format='newline_delimited',
+                 columns = {time: 'timestamptz', power: 'int32'});
+
 CREATE OR REPLACE VIEW daily_view as SELECT date, round(sum(power * dur)/(60*60)) as total_watt_hours
 FROM (SELECT power, date_trunc('day', time at time zone 'EST') as date, 
             epoch(time - lag(time) over (ORDER BY TIME)) as dur_internal,
             case when dur_internal > 60 then 60 else dur_internal END as dur 
       from (SELECT * from powerlog UNION ALL 
-            SELECT * from read_json_auto('/home/dga/solar/log.json', format='newline_delimited',
-                 columns = {time: 'timestamptz', power: 'int32'})))
+            SELECT * from solar_log_json))
 WHERE dur is not null and power >= 4
 GROUP BY date
 ORDER BY date DESC;
@@ -13,9 +16,7 @@ CREATE OR REPLACE VIEW current_view as SELECT date, round(sum(power * dur)/(60*6
 FROM (SELECT power, date_trunc('day', time at time zone 'EST') as date, 
             epoch(time - lag(time) over (ORDER BY TIME)) as dur_internal,
             case when dur_internal > 60 then 60 else dur_internal END as dur 
-      from (
-            SELECT * from read_json_auto('/home/dga/solar/log.json', format='newline_delimited',
-                 columns = {time: 'timestamptz', power: 'int32'})))
+      from solar_log_json)
 WHERE dur is not null and power >= 4
 GROUP BY date
 ORDER BY date DESC;
