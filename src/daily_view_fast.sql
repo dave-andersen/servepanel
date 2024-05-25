d230b5e4c4814c31309c832568f9be7be22ea8bc
David G. Andersen <dave.andersen@gmail.com>
2024-05-25 01:27:33 +0000

    fix current day mismatch bug in view and speed it up
    note: depends on the json ingest happening when
    the sun isn't shining. which is what happens.

--- src/daily_view_fast.sql
+++ src/daily_view_fast.sql
@@ -1,2 +1,5 @@
 CREATE OR REPLACE VIEW daily_view_fast_string as
-select strftime(date, '%Y-%m-%d') as date, total_watt_hours from (SELECT * FROM daily_view where date==current_date UNION ALL SELECT * FROM materialized_daily) ORDER BY date DESC;
+select strftime(date, '%Y-%m-%d') as date, total_watt_hours 
+FROM (SELECT * FROM current_view where date > (SELECT MAX(date) from materialized_daily)
+      UNION ALL 
+      SELECT * FROM materialized_daily) ORDER BY date DESC;
